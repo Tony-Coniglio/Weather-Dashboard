40bd4028f2d6d2507c8742c48aec129df349c58b
Tony-Coniglio <113558159+Tony-Coniglio@users.noreply.github.com>
2022-11-11 13:26:10 -0600

    started API

---
+++ Main/assets/js/script.js
@@ -0,0 +1,6 @@
+var requestURL = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=17704f7771dd1f3e4ced930e7a42c1cd';
+
+var locationLat
+var locationLon
+
+function getCityLocation (event, )
